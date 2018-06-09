e022faf5f5b33b2f40b18198f0e7c134526f0e9d
dnguyen1196 <ducnguyenmanh96@gmail.com>
2018-06-09 09:38:39 -0400

    got some results for count

--- Tensor/Tensor.py
+++ Tensor/Tensor.py
@@ -27,7 +27,6 @@ class tensor(object):
 
     # synthesize data according to some model
     def synthesize(self, dims, means, covariances, D=10, train=0.8, sparsity=0.5):
-
         """
         :param dims:
         :param means:
