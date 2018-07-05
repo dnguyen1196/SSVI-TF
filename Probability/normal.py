finally fixed robust stupid minus sign in normal pdf

Probability/normal.py
@@ -15,9 +15,12 @@ class NormalDistribution(object):
         return np.maximum(res, self.epsilon)
 
     def fst_derivative_pdf(self, y, m, s):
-        return -np.multiply(self.pdf(y, m, s), (y - m) / s)
+        # res = -np.multiply(self.pdf(y, m, s), (y - m) / np.square(s))
+        res  = np.multiply(self.pdf(y, m, s), (y - m) / np.square(s))
+        return res
 
     def snd_derivative_pdf(self, y, m, s):
+        res = np.multiply(self.pdf(y, m, s), (np.square((y-m)/s**4) - 1/s**2))
         return np.multiply(self.pdf(y, m, s), (np.square((y-m)/s) - 1/s))
 
     def log_pdf(self, y, m, s):

SSVI/SSVI_TF_interface.py
@@ -82,7 +82,7 @@ class SSVI_TF(object):
 
     def factorize(self, report=1000):
         self.report = report
-        update_column_pointer = [0] * self.order
+        update_column_pointer = [0 for _ in range(self.order)]
         start = time.time()
         iteration = 0
 
@@ -95,14 +95,15 @@ class SSVI_TF(object):
                 # in the dim-th dimension
 
                 # self.update_natural_params(dim, col, iteration)
-                self.update_natural_param_batch(dim, col, iteration)
+                self.update_natural_param_batch(dim, col)
 
-                self.update_hyper_parameter(dim, iteration)
+                self.update_hyper_parameter(dim)
 
             # Move on to the next column of the hidden matrices
             for dim in range(self.order):
                 if (update_column_pointer[dim] + 1 == self.dims[dim]):
                     self.time_step[dim] += 1  # increase time step
+
                 update_column_pointer[dim] = (update_column_pointer[dim] + 1) \
                                              % self.dims[dim]
 
@@ -115,9 +116,8 @@ class SSVI_TF(object):
                 break
 
             iteration += 1
-        return
 
-    def update_hyper_parameter(self, dim, iteration):
+    def update_hyper_parameter(self, dim):
         """
         :param dim:
         :return:
@@ -130,14 +130,14 @@ class SSVI_TF(object):
 
         self.pSigma[dim] = sigma/(M*self.D)
 
-    def update_natural_param_batch(self, dim, i, iteration):
+    def update_natural_param_batch(self, dim, i):
         observed = self.tensor.find_observed_ui(dim, i)
 
         if len(observed) > self.batch_size:
             observed_idx = np.random.choice(len(observed), self.batch_size, replace=False)
             observed_subset = np.take(observed, observed_idx, axis=0)
         else:
-            observed_subset = np.copy(observed)
+            observed_subset = observed
 
         (m, S) = self.posterior.get_vector_distribution(dim, i)
         coords = np.array([entry[0] for entry in observed_subset])
@@ -303,6 +303,7 @@ class SSVI_TF(object):
                     vj_sample = np.random.multivariate_normal(mi, np.diag(Si), size=k)
                 else:
                     vj_sample = np.random.multivariate_normal(mi, Si, size=k)
+                # print (vj_sample.shape)
 
                 vjs_batch[num, :, :] = np.multiply(vjs_batch[num, :, :], vj_sample)
 

SSVI/SSVI_TF_robust.py
@@ -44,17 +44,20 @@ class SSVI_TF_robust(SSVI_TF):
         # print("vjs ", vjs_batch.shape)
 
         # uis_batch.shape = (num_samples, k1, D)
+        # ....
+        # TODO: check dimensionality!!
         uis_batch = np.random.multivariate_normal(m, S, size=(num_subsamples,self.k1))
-        # TODO: check if this is correct
-        # print(uis_batch.shape)
 
-        assert(num_subsamples == np.size(uis_batch, axis=0)) # sanity check
-        assert(num_subsamples == np.size(vjs_batch, axis=0)) # sanity check
+        assert(uis_batch.shape == vjs_batch.shape)
+
+        # assert(num_subsamples == np.size(uis_batch, axis=0)) # sanity check
+        # assert(num_subsamples == np.size(vjs_batch, axis=0)) # sanity check
 
         ws_batch   = np.random.rayleigh(np.square(self.w_sigma), size=(num_subsamples, self.k1))
 
         # mean_batch.shape = (num_samples, k1)
         mean_batch = np.sum(np.multiply(vjs_batch, uis_batch), axis=2)
+        assert(mean_batch.shape == (num_subsamples, self.k1))
 
         di, Di, si = self.approximate_di_Di_si_with_second_layer_samplings(ys, mean_batch, vjs_batch, ws_batch)
 
@@ -88,23 +91,20 @@ class SSVI_TF_robust(SSVI_TF):
             v  = vjs_batch[num, :, :] # v.shape = (k1, D)
             w  = ws_batch[num, :]     # w.shape = (k1,)
 
-            di[num, :] = np.average(np.transpose(np.multiply(np.transpose(v), \
+            di[num, :] = np.mean(np.transpose(np.multiply(np.transpose(v), \
                                                              1/p * p1)), axis=0)
 
-            si[num]    = np.average(np.multiply(w, np.divide(p2, p))) / (8*np.square(self.w_sigma))
+            si[num]    = np.mean(w * p2 / p) / (8*np.square(self.w_sigma))
 
             for k in range(self.k1):
-                Di[num, :, :] += 0.5 / self.k1 /p[k] * \
+                Di[num, :, :] += 0.5 / self.k1 / p[k] * \
                                  (np.outer(v[k, :], v[k, :]) * p2[k] \
-                                  - 1/p[k] * np.outer(v[k]*p2[k], v[k]*p2[k]))
+                                  - 1/p[k] * np.outer(v[k, :]*p2[k], v[k, :]*p2[k]))
 
         di = np.sum(di, axis=0)
         Di = np.sum(Di, axis=0)
         si = np.sum(si)
 
-        # print("di.shape ", di.shape)
-        # print("Di.shape ", Di.shape)
-        # print("si.shape ", si.shape)
         return di, Di, si
 
     def estimate_expected_derivatives_pdf_batch(self, ys, mean_batch, ws_batch):
@@ -130,14 +130,13 @@ class SSVI_TF_robust(SSVI_TF):
             # For each num_samples, fs.shape = (k2, k1)
             fs = np.random.normal(mean_batch[num], ws_batch[num], size=(self.k2, self.k1))
 
-            pdf[num, :]       = np.average(self.likelihood.pdf(ys[num], fs, s), axis=0)
-            fst_deriv[num, :] = np.average(self.likelihood.fst_derivative_pdf(ys[num], fs, s), axis=0)
-            snd_deriv[num, :] = np.average(self.likelihood.snd_derivative_pdf(ys[num], fs, s), axis=0)
+            pdf[num, :]       = np.mean(self.likelihood.pdf(ys[num], fs, s), axis=0)
+            fst_deriv[num, :] = np.mean(self.likelihood.fst_derivative_pdf(ys[num], fs, s), axis=0)
+            snd_deriv[num, :] = np.mean(self.likelihood.snd_derivative_pdf(ys[num], fs, s), axis=0)
 
         # assert (pdf.shape == (num_samples, self.k1))
         # assert (fst_deriv.shape == (num_samples, self.k1))
         # assert (snd_deriv.shape == (num_samples, self.k1))
-
         return pdf, fst_deriv, snd_deriv
 
     # TODO: implement closed form version

Test/SSVI_robust/real/PTF_2D_robust.py
@@ -7,7 +7,7 @@ np.random.seed(seed=317)
 # For control and comparison
 # Generate synthesize data, true, this is what we try to recover
 
-dims     = [50, 50]
+dims     = [10, 10]
 hidden_D = 20
 means    = [np.ones((hidden_D,)) * 5, np.ones((hidden_D,)) * 10]
 covariances = [np.eye(hidden_D) *2, np.eye(hidden_D) * 3]
@@ -22,13 +22,13 @@ D = 20
 mean0 = np.ones((D,)) * 5
 cov0 = np.eye(D)
 
-mean_update = "N"
-cov_update  = "S"
+mean_update = "S"
+cov_update  = "N"
 
 factorizer  = SSVI_TF_robust(data, rank=D, \
                             mean_update=mean_update, cov_update=cov_update, \
                             mean0=mean0, cov0=cov0,\
-                            k1=256, k2=128, \
-                            window_size=10, eta=1, cov_eta=1, sigma_eta=1)
+                            k1=256, k2=64, \
+                            window_size=10, eta=1, cov_eta=0.01, sigma_eta=1)
 
-factorizer.factorize(report=50)
+factorizer.factorize(report=5)