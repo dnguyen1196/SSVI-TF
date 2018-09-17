a0c391cfa47745e913b876a12602b2b70d89014e
dnguyen1196 <ducnguyenmanh96@gmail.com>
2018-09-17 12:01:44 -0400

    some new results and code changes

--- learn_curve_dir.sh
+++ learn_curve_dir.sh
@@ -1,36 +1,22 @@
 #!/bin/bash
-module load python/3.5.0
 
 opts="-p batch -c 2 --mem=4196 --time=4-00:00:00 --mail-type=ALL --mail-user=$USER"
 
 read outdir # output directory
-read diag   # diagonal formulation
-read noise  # noise level
-read ratio  # using ratio
 
 echo "Output directory: $outdir"
-echo "Using diagonal covariance? $diag"
-echo "Noise level: $noise"
-echo "Using noise as ratio?: $ratio"
+script="./venv/bin/python -u learning_curve.py --rand -dim 50 50 50"
+
+# NOTE: train size
+# NOTE: --matrix
+# NOTE: diag
+# NOTE: dimension
 
 while IFS=' ' read -r data model
 do
     outs="--output=$outdir$data-$model.out --error=$outdir$data-$model.err "
 
-    if [ "$diag" == "true" ]
-    then choose_diag="--diag"
-    else choose_diag=""
-    fi
-
-    if [ "$ratio" == "true" ]
-    then choose_ratio="--ratio"
-    else choose_ratio=""
-    fi
-
-    wrapper="python learning_curve.py -d $data -m $model -n $noise $choose_diag $choose_ratio"
-    echo "$wrapper"
-    #sbatch $opts $outs --wrap="$wrapper"
-    sbatch $opts $outs --wrap="python learning_curve.py -d $data -m $model -n $noise $choose_diag $choose_ratio"
+    sbatch $opts $outs --wrap="$script -d $data -m $model"
 
     sleep 1
 done
--- learning_curve.py
+++ learning_curve.py
@@ -17,17 +17,12 @@ warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
 
 np.random.seed(seed=319)
 
-default_params = {"mean_update" : "S", "cov_update" : "N", "rank" : 20, "k1" : 64, "k2" : 64}
+default_params = {"mean_update" : "S", "cov_update" : "N", "rank" : 20, "k1" : 64, "k2" : 64, "eta":1, "cov_eta":1, "randstart":True}
+
 
 def get_factorizer_param(model, datatype, diag):
-    if model == "deterministic" or model == "simple":
-        return  {"eta" : 1, "cov_eta" : 1}
+    return default_params
 
-    # TODO: what about robust and diag? do I need sigma_eta
-    if model == "robust":
-        return  {"eta" : 1, "cov_eta" : 0.01 if datatype == "real" else 0.001, "sigma_eta" : 0.01, \
-                 "unstable_cov" : (True if not diag else False)}
-    return None
 
 def get_init_values(datatype, D):
     cov0 = np.eye(D)
@@ -37,8 +32,8 @@ def get_init_values(datatype, D):
         mean0 = np.zeros((D,))
     return {"cov0" : cov0, "mean0" : mean0}
 
-def synthesize_tensor(datatype, noise, noise_ratio):
-    dims = [50, 50, 50]
+
+def synthesize_tensor(datatype, dims, noise, noise_ratio):
     real_dim = 100
     means = [np.ones((real_dim,)) * 5, np.ones((real_dim,)) * 10, np.ones((real_dim,)) * 2]
     covariances = [np.eye(real_dim) * 2, np.eye(real_dim) * 3, np.eye(real_dim) * 2]
@@ -54,25 +49,34 @@ def synthesize_tensor(datatype, noise, noise_ratio):
                            train=0.8, sparsity=1, noise=noise, noise_ratio=noise_ratio)
     return tensor
 
-def do_learning_curve(factorizer, tensor, iter_num):
+
+def do_learning_curve(factorizer,  tensor, iter_num):
+    # Perform learning curve by exposing the algorithm to varying percentage
+    # of training data
     train_curve = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1]
     for size in train_curve:
         print("Using ", size, " of training data ")
         tensor.reduce_train_size(size)
         factorizer.reset()
-        factorizer.factorize(report=500, max_iteration=iter_num)
+        factorizer.factorize(report=500, max_iteration=iter_num, to_report=[0, 10, 25])
 
-def test_learning_curve(datatype, model, diag, noise, iter_num, noise_ratio):
-    D = 20
-    tensor = synthesize_tensor(datatype, noise, noise_ratio)
-    factorizer_param = get_factorizer_param(model, datatype, diag)
-    init_vals = get_init_values(datatype, D)
-    params = {**default_params, **factorizer_param, **init_vals, "tensor": tensor}
 
-    if diag:
-        params["diag"] = True
+def test_learning_curve(datatype, model, diag, noise, iter_num, noise_ratio, dims):
+    # The first step is to synthesize the tensor
+    # according to the given dimension and datatype and noise
+    if noise_ratio is not None:
+        noise_ratio = True
+        noise = noise_ratio
     else:
-        params["diag"] = False
+        noise_ratio = False
+    tensor = synthesize_tensor(datatype, dims, noise, noise_ratio)
+
+    # Next step is to initialize the factorizer with appropriate
+    # parameters
+    D = 20
+    init_vals = get_init_values(datatype, D)
+    params = {**default_params, **init_vals, "tensor": tensor}
+    params["diag"] = diag
 
     if model == "deterministic":
         factorizer = SSVI_TF_d(**params)
@@ -81,52 +85,47 @@ def test_learning_curve(datatype, model, diag, noise, iter_num, noise_ratio):
     elif model == "robust":
         factorizer = SSVI_TF_robust(**params)
 
+    # Evaluate the 'true' model
     factorizer.evaluate_true_params()
+
+    # Perform learning curve
     do_learning_curve(factorizer, tensor, iter_num)
 
 
-parser = argparse.ArgumentParser(description='3D tensor factorization synthetic data')
-parser.add_argument("-d", "--data", type=str, help="data types: binary, real or count")
-parser.add_argument("-m", "--model", type=str, help="model: simple, deterministic or robust")
+
+parser = argparse.ArgumentParser(description="Testing models at specific training size")
+parser.add_argument("-m", "--model", type=str, help="model of factorizer", choices=["deterministic", "simple", "robust"])
+parser.add_argument("-d", "--datatype", type=str, help="datatype of tensor", choices=["real", "binary", "count"])
+
+excl_group = parser.add_mutually_exclusive_group()
+excl_group.add_argument("-r", "--ratio", type=float, help="noise as ratio")
+excl_group.add_argument("-n", "--noise", type=float, help="noise level", default=0.0)
+
 parser.add_argument("--diag", action="store_true")
-parser.add_argument("-n", "--noise", type=float, help="noise level")
-parser.add_argument("-i", "--iteration", type=int, help="number of iterations")
-parser.add_argument("-r", "--ratio", action="store_true")
-parser.add_argument("-o", "--output", type=str, help="output folder")
+parser.add_argument("-tr", "--train_size", type=float, help="portion of training data")
+parser.add_argument("--fixed_cov", action="store_true", help="Fixed covariance")
+parser.add_argument("-it", "--num_iters", type=int, help="Max number of iterations", default=10000)
+parser.add_argument("-re", "--report", type=int, help="Report interval", default=500)
+parser.add_argument("--quadrature", action="store_true", help="using quadrature")
+parser.add_argument("--matrix", action="store_true", help="Doing matrix factorization instead of tensor factorization")
+parser.add_argument("-ceta", "--cov_eta", type=float, help="cov eta", default=1.0)
+parser.add_argument("--rand", action="store_true", help="Using random start")
+parser.add_argument("-meta", "--mean_eta", type=float, help="mean eta", default=1.0)
+parser.add_argument("-dim", "--dimension", nargs='+', required=True, default=[50, 50, 50])
+parser.add_argument("-k1", "--k1", type=int, help="k1 samples", default=64)
+parser.add_argument("-k2", "--k2", type=int, help="k2 samples", default=128)
 
 args = parser.parse_args()
 
-datatype = args.data
+datatype = args.datatype
 model    = args.model
 diag     = args.diag
 noise    = args.noise
-iter_num = args.iteration
+iter_num = args.num_iters
 noise_ratio = args.ratio
-outfolder   = args.output
+dims = [int(x) for x in args.dimension]
 
-output     = outfolder + "{}_{}_out.txt".format(model, datatype)
-sys.stdout = open(output, "w")
-sys.stdout.flush()
-
-if noise is None:
-    noise = 0.1
-
-if not noise_ratio:
-    if datatype == "real":
-        noise = 500
-    elif datatype == "binary":
-        noise = 0.5
-    elif datatype == "count":
-        noise = 1
-    noise_ratio = False    
-
-if iter_num is None:
-    iter_num = 6000
-
-assert (datatype in ["binary", "real", "count"])
-assert (model in ["simple", "deterministic", "robust"])
-
-test_learning_curve(datatype, model, diag, noise, iter_num, noise_ratio)
+test_learning_curve(datatype, model, diag, noise, iter_num, noise_ratio, dims)
 
 
 """
