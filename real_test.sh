7af65d7820e55de58b589bdaa24a3fad6a5d9b61
ducnguyentufts <manh.nguyen@tufts.edu>
2018-12-27 15:39:46 +0700

    to test on HPC

--- real_test.sh
+++ real_test.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
-#module load python/3.5.0
-
+module load python/3.5.0
 
 opts="-p batch -c 2 --mem=16000 --time=4-00:00:00 --mail-type=ALL --mail-user=$USER"
 
 read outdir # output directory
+read filename # Filename
+read datatype # datatype
 echo "Output directory: $outdir"
+echo "Filename: $filename" 
 
-script="./venv/bin/python -u factorize.py --diag -f ../tensor-data/GDELT2011/GDELT.2011.REDUCED.cleaned.txt -d count -re 50 -iter 50000"
+script="python -u real_test.py --diag -f Baseline/Data/$filename -d $datatype"
 
 while IFS=' ' read -r model
 do
     outs="--output=$outdir$model.out --error=$outdir$model.err"
-
     sbatch $opts $outs --wrap="$script -m $model"
-
     sleep 1
 done
