03df42ea52516b0cf2dbda34cfde3987f7a35ffe
ducnguyentufts <manh.nguyen@tufts.edu>
2018-12-30 17:27:49 +0700

    modified binary data file

--- Baseline/Data/matlab_to_tensor.py
+++ Baseline/Data/matlab_to_tensor.py
@@ -18,13 +18,14 @@ from Tensor.count_tensor import count_tensor
 parser = argparse.ArgumentParser(description="Transforming from matlab .mat file to text file")
 parser.add_argument("-f", "--file", type=str, help=".mat file", required=True)
 parser.add_argument("-o", "--output", type=str, help="output text file", required=True)
-# parser.add_argument("-d", "--datatype", type=str, help="tensor entry data type", choices=["binary", "count", "real"])
+parser.add_argument("-d", "--datatype", type=str, help="tensor entry data type", choices=["binary", "count", "real"])
 
 
 args = parser.parse_args()
 
 inputf = args.file
 output = args.output
+datatype = args.datatype
 
 assert(".mat" in inputf)
 
@@ -39,6 +40,8 @@ num_entries    = xi.shape[0]
 size_per_dimension = [max(ids[0][j])[0] for j in range(num_dimensions)]
 count = 0
 
+# fill in the blanks for the binary data?
+
 with open(output, "w") as f:
 	f.write(",".join([str(x) for x in size_per_dimension]))
 	f.write("\n")
@@ -51,6 +54,17 @@ with open(output, "w") as f:
 		f.write(out)
 		f.write("\n")
 		
+		relation_dim = 2
+		if datatype == "binary": # If data is binary, augment [id1, id2, not id3] -> 0
+			max_dim3 = size_per_dimension[relation_dim]
+			for k in range(max_dim3):
+				if k != coordinates[relation_dim]:
+					neg_coordinates = coordinates[:-1] + [k]
+					out = ",".join([str(x) for x in neg_coordinates]) + "," + str(0)
+					f.write(out)
+					f.write("\n")
+
+
 		# count += 1
 		# if count == 10:
 		# 	break
